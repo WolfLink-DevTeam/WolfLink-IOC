81b4fddfb670caba39d9638d8c619fed1da39b40
MikkoAyaka <3401286177@qq.com>
2023-06-25 00:20:51 +0800

    初始化 git 信息

---
+++ src/main/java/org/wolflink/common/ioc/Singleton.java
@@ -0,0 +1,8 @@
+package org.wolflink.common.ioc;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Singleton {
+}
